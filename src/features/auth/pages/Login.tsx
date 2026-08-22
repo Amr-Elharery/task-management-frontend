385918ae07783a08df3142c72b7f1e5a77d42ee7
Amr Elharery <amrelharery@gmail.com>
2026-08-22 16:31:58 +0300

    Fix unused imports

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from 'react-router';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Eye, EyeOff } from 'lucide-react';
 
 import {
